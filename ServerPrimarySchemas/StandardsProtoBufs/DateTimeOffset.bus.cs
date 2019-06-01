00bab8f3aa8107df14668d8bf2ea0c24d47d5830
thorhalbert <thorhalbert@gmail.com>
2019-06-01 12:40:34 -0700

    Generate the protobuf classes into the right directory (check the csproj file). Also wrote a wrapper business partial class for the Guid and DateTimeOffset types.
    Protobuf seems to be coming together.

---
+++ ServerPrimarySchemas/StandardsProtoBufs/DateTimeOffset.bus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardsProto
+{
+    public sealed partial class ProtoDateTimeOffset
+    {
+        public DateTimeOffset RealDate
+        {
+            get {
+                var tick = this.Ticks;
+                var offset = this.Offset;
+
+                return new DateTimeOffset(ticks: tick, new TimeSpan(0, offset, 0));         
+            }
+            set
+            {
+                this.Ticks = value.Ticks;
+                this.Offset = Convert.ToInt32(value.Offset.TotalMinutes);
+            }
+        }
+    }
+}
---
+++ ServerPrimarySchemas/StandardsProtoBufs/GUID.bus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace StandardsProto
+{
+    public sealed partial class ProtoGuid
+    {
+        public Guid RealGuid
+        {
+            get
+            {
+                byte[] outArray = new byte[16];
+                this.UuidBytes.CopyTo(outArray, 0);
+                return new Guid(outArray);
+            }
+            set
+            {
+                UuidBytes = Google.Protobuf.ByteString.CopyFrom(value.ToByteArray());
+            }
+        }
+    }
+}
