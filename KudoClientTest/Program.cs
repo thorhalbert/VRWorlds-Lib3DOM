1696aa12e135f19292e4acc3e148937ee8e59219
thorhalbert <thorhalbert@gmail.com>
2019-06-14 20:55:14 -0700

    Made a small ping server.  Sucessful ping...

---
+++ KudoClientTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+using VRWorlds.Schemas.Proto.Common;
+
+namespace KudoClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("thorcelap5:50051", ChannelCredentials.Insecure);
+
+            var client = new Ping.PingClient(channel);
+
+            var reply = client.Ping(new PingRequest());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
