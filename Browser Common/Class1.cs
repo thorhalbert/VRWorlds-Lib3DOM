2c797f2c9b0a18103789aea9e3823f5e974a1be1
Thor Halbert <thorhalbert@gmail.com>
2019-07-27 10:08:37 -0700

    More Cleanup

---
+++ Browser Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Browser_Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ General Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace General_Common
+{
+    public class Class1
+    {
+    }
+}
