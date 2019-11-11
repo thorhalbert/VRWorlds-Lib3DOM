ff92b79f5cfdc9b83c595bd7e27297074a61ee4e
Thor Halbert <thorhalbert@gmail.com>
2019-11-10 16:20:07 -0800

    Start structuring the servers, including a big new authentiation/authorization system.

---
+++ Servers/Kudo/KudoAuthServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KudoAuthServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
